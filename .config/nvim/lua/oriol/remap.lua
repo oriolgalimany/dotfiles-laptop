a82892f428864a0d697f4b71378181a2db229ac9
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2023-02-19 19:40:05 +0100

    added nvim config files

---
+++ .config/nvim/lua/oriol/remap.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
+
+-- disable esc in insert mode
+vim.keymap.set("i", "<esc>", "<nop>")
+-- esc bind
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("i", "kj", "<esc>")
+
+-- clear search highlights
+vim.keymap.set("n", "<leader>n", ":noh<CR>")
+
+-- test
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
