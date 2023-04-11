47c9872f4fb688803aa12555d01d1ca55fdaf38b
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2023-04-11 16:59:22 +0200

    fixed lsp config

--- .config/nvim/lua/oriol/plugin/lsp.lua
+++ .config/nvim/lua/oriol/plugin/lsp.lua
@@ -15,3 +15,24 @@ lsp.set_preferences({
 })
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+vim.g.diagnostics_visible = true
+function _G.toggle_diagnostics()
+  if vim.g.diagnostics_visible then
+    vim.g.diagnostics_visible = false
+    vim.diagnostic.disable()
+  else
+    vim.g.diagnostics_visible = true
+    vim.diagnostic.enable()
+  end
+end
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>d', ':call v:lua.toggle_diagnostics()<CR>', {silent=true, noremap=true})
