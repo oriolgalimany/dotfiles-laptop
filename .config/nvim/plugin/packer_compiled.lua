32665261401bc00883d6ffd6b2b8b307f09f3882
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2023-05-08 23:47:06 +0200

    added autocmd.lua

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -226,18 +226,18 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: night-owl
-time([[Config for night-owl]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme night-owl\bcmd\bvim\0", "config", "night-owl")
-time([[Config for night-owl]], false)
--- Config for: nvimgelion
-time([[Config for nvimgelion]], true)
-try_loadstring("\27LJ\2\n\v\0\0\1\0\0\0\1K\0\1\0\0", "config", "nvimgelion")
-time([[Config for nvimgelion]], false)
 -- Config for: tokyonight
 time([[Config for tokyonight]], true)
 try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\27colorscheme tokyonight\bcmd\bvim\0", "config", "tokyonight")
 time([[Config for tokyonight]], false)
+-- Config for: nvimgelion
+time([[Config for nvimgelion]], true)
+try_loadstring("\27LJ\2\n\v\0\0\1\0\0\0\1K\0\1\0\0", "config", "nvimgelion")
+time([[Config for nvimgelion]], false)
+-- Config for: night-owl
+time([[Config for night-owl]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme night-owl\bcmd\bvim\0", "config", "night-owl")
+time([[Config for night-owl]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
