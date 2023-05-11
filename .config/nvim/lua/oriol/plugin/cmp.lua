b2dcd35e9c8992a0ca0a3ca3f97923d27bf4aa38
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2023-05-11 17:58:07 +0200

    added cmp options

---
+++ .config/nvim/lua/oriol/plugin/cmp.lua
@@ -0,0 +1,17 @@
+-- Make sure you setup `cmp` after lsp-zero
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  },
+  formatting = {
+    fields = {'abbr', 'kind', 'menu'},
+    format = require('lspkind').cmp_format({
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+    })
+  }
+})
