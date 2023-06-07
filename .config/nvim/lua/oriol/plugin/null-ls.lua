542b6e92c66bb4a028f4d1e2e10fcfe20c10d4fb
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2023-06-07 02:24:49 +0200

    added formatting

---
+++ .config/nvim/lua/oriol/plugin/null-ls.lua
@@ -0,0 +1,17 @@
+-- check for builtin linters and formatters:
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+-- and install them using mason
+
+
+local lsp = require('lsp-zero')
+local null_ls = require('null-ls')
+local null_opts = lsp.build_options('null-ls', {})
+
+null_ls.setup({
+    on_attach = function(client, bufnr)
+        null_opts.on_attach(client, bufnr)
+    end,
+    sources = {
+        null_ls.builtins.formatting.prettierd,
+    }
+})
