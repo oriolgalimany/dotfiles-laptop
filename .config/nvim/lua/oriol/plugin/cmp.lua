66a495b09e5a0e68bf68f3f504a293f70d8d12e7
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2023-07-24 02:28:50 +0200

    fixed cmp

--- .config/nvim/lua/oriol/plugin/cmp.lua
+++ .config/nvim/lua/oriol/plugin/cmp.lua
@@ -3,16 +3,24 @@
 local cmp = require('cmp')
 
 cmp.setup({
-  mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-  },
-  formatting = {
-    fields = {'abbr', 'kind', 'menu'},
-    format = require('lspkind').cmp_format({
-      mode = 'symbol_text', -- show only symbol annotations
-      preset = 'codicons', -- requires codicons.ttf vscode font
-      maxwidth = 50, -- prevent the popup from showing more than provided characters
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-    })
-  }
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    },
+    formatting = {
+        fields = { 'abbr', 'kind', 'menu' },
+        format = require('lspkind').cmp_format({
+            mode = 'symbol_text',  -- show only symbol annotations
+            preset = 'codicons',   -- requires codicons.ttf vscode font
+            maxwidth = 50,         -- prevent the popup from showing more than provided characters
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+        })
+    },
+    sources = { -- both sources needed to lsp signature work with lsp
+        { name = "nvim_lsp" },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'path' },
+        { name = 'buffer' }
+    }
 })
