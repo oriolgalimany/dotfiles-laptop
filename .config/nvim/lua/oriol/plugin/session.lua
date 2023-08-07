77afe1e7c969341d5c044bbd99bd5bd31fbd05ee
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2023-08-07 23:48:05 +0200

    auto-session plugin

--- .config/nvim/lua/oriol/plugin/session.lua
+++ .config/nvim/lua/oriol/plugin/session.lua
@@ -1,1 +1,5 @@
-vim.g.prosession_dir = '~/.local/share/nvim/session/'
+require('auto-session').setup {
+    auto_session_root_dir = '~/.local/share/nvim/session/',
+    auto_session_suppress_dirs = { "~/", "~/Projects", "~/Baixades", "/" },
+    auto_restore_enabled = true,
+}
