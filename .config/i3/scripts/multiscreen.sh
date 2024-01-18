8ce06ae244ae7e0cc556c329164122a67281cd3e
oriol.galimany@gmail.com <oriol.galimany@gmail.com>
2024-01-18 02:41:12 +0100

    added multiscreen script

---
+++ .config/i3/scripts/multiscreen.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+displays_info=$(xrandr | grep 'DisplayPort.* connected' -A 1)
+
+ports=()
+resolutions=()
+
+while IFS= read -r line; do
+    if [[ $line == *"DisplayPort"* ]]; then
+        ports+=($(echo "$line" | awk '{print $1}'))
+    elif [[ $line == *"+"* ]]; then
+        resolutions+=($(echo "$line" | awk '{print $1}'))
+    fi
+done <<< "$displays_info"
+
+if [[ ${#ports[@]} -eq 0 ]]; then
+    # echo "No s'han trobat monitors DisplayPort. Sortint del script."
+    exit 1
+fi
+
+primary_added=false
+xrandr_command="xrandr"
+previous_port=""
+
+all_ports=($(xrandr | grep -E '\bconnected\b' | awk '{print $1}'))
+
+for port in "${all_ports[@]}"; do
+    if [[ ! " ${ports[@]} " =~ " ${port} " ]]; then
+        # Disable unused ports
+        xrandr_command+=" --output $port --off"
+    fi
+done
+
+for i in "${!ports[@]}"; do
+    xrandr_command+=" --output ${ports[$i]} --mode ${resolutions[$i]}"
+
+    if [[ -n $previous_port ]]; then
+        xrandr_command+=" --right-of $previous_port"
+    fi
+
+    if [[ ${resolutions[$i]} == "1920x1080" && $primary_added == false ]]; then
+        xrandr_command+=" --primary"
+        primary_added=true
+    fi
+
+    previous_port="${ports[$i]}"
+done
+
+eval "$xrandr_command"
